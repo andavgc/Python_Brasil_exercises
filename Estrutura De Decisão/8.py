354a5ce6b2824f05d4b3f866b7350e839e54e7d1
andav <andav_@gitlab.com>
2023-01-28 16:03:14 -0300

    some corrections

--- Estrutura De Decisão/8.py
+++ Estrutura De Decisão/8.py
@@ -1,26 +1,15 @@
 #Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,
 #sabendo que a decisão é sempre pelo mais barato.
 
-lista = {}
 produto = []
-precos = []
+mais_barato = None
 num = 0
 
-for preco in range(3):
+for i in range(3):
     num = num + 1
     produto = input("ingresse o %dº produto: " % num)
-#    items.append(item)
     preco = int(input("ingresse o preço do produto em R$: "))
-#    precos.append(preco)
-    lista[produto] = preco
+    if mais_barato is None or mais_barato[0] > preco:
+        mais_barato = (preco, produto)
 
-print(lista)
-
-lista_o = sorted(lista.items(), key=lambda item: item[1])
-
-print(lista_o[0][1])
-for a, b in lista_o:
-    print(a, b)
-    barato = a
-    print("Você deve comprar %s"% barato)
-    break
+print("Você deve comprar %s"% mais_barato[1])
